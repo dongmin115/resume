d7ed1ba3ab9289f894db9733dd8c095d67a7dcf9
dongmin <dongmin11566@gmail.com>
2024-02-17 17:26:06 +0900

    fix : pixelratio 수정하여 계단현상 해결

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -4,18 +4,63 @@ export default function Tech() {
     useEffect(() => {
     const canvas = document.getElementById('tech_canvas') as HTMLCanvasElement;
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-    const x = canvas.width / 2;
-    const y = canvas.height / 2;
-    const radius = Math.min(canvas.width, canvas.height) / 4;
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    ctx.closePath();
+    const circles: { x: number, y: number, radius: number, speed: number }[] = [];
+
+    function setCanvasSize() {
+        const dpr = window.devicePixelRatio;
+        const rect = canvas.getBoundingClientRect();
+        canvas.width = rect.width * dpr;
+        canvas.height = rect.height * dpr;
+        ctx.scale(dpr, dpr);
+        animate();
+    }
+
+    function drawCircle(x: number, y: number, radius: number) {
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, Math.PI * 2);
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+      ctx.fill();
+      ctx.closePath();
+    }
+
+    function updateCircles() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      for (let i = 0; i < circles.length; i++) {
+        const circle = circles[i];
+        circle.y -= circle.speed;
+        if (circle.y < -circle.radius) {
+          circle.y = canvas.height + circle.radius;
+        }
+        drawCircle(circle.x, circle.y, circle.radius);
+      }
+    }
+
+    function createCircle() {
+        if(circles.length < 20){
+        const x = Math.random() * canvas.width;
+        const y = canvas.height + Math.random() * canvas.height;
+        const radius = Math.random() * 1 + 5;
+        const speed = Math.random() * 0.5;
+        circles.push({ x, y, radius, speed });
+        }
+    }
+
+    function animate() {
+      createCircle();
+      updateCircles();
+      return requestAnimationFrame(animate);
+    }
+    
+    setCanvasSize();
+    animate();   
+
+    return () => {
+        cancelAnimationFrame(animate());
+    };
     },[])
     return (
         <div className="w-screen h-screen">
-            <canvas id="tech_canvas" className="w-full h-full fixed top-0 left-0 -z-10" />
+            <canvas id="tech_canvas" className="fixed top-0 left-0 -z-10" />
             <div className="flex justify-between items-center w-full h-full flex-col">
                 <div className="text-5xl basis-[10%] w-fit items-center flex justify-center">Tech</div>
                 <div className="w-full flex flex-col basis-[90%] justify-evenly items-center">
