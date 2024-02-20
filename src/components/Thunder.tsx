cde373b44b404b5945e09e28ad3756be9c5bf082
dongmin <dongmin11566@gmail.com>
2024-02-20 15:50:49 +0900

    feat : threeJS  카메라, 장면, 렌더러 추가

--- src/components/Thunder.tsx
+++ src/components/Thunder.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 
 export default function Thunder() {
     
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current as HTMLCanvasElement;
+
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        
+        const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    },[])
+    
     
     return (
-        <div className='w-screen h-screen bg-black'></div>
+        <div className='w-screen h-screen bg-black'>
+            <canvas ref={canvasRef}/>
+        </div>
     )
 }
