b3b6af0ebecc928bfe8a131445ce465eccfc3ba6
dongmin <dongmin11566@gmail.com>
2024-02-20 16:00:59 +0900

    feat : three.js를 이용한 3D 효과 추가

--- src/components/Thunder.tsx
+++ src/components/Thunder.tsx
@@ -6,6 +6,7 @@ export default function Thunder() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
+        //초기세팅
         const canvas = canvasRef.current as HTMLCanvasElement;
 
         const scene = new THREE.Scene();
@@ -13,11 +14,27 @@ export default function Thunder() {
         
         const renderer = new THREE.WebGLRenderer({ canvas: canvas });
         renderer.setSize(window.innerWidth, window.innerHeight);
+
+        //큐브생성
+        const geomertry = new THREE.BoxGeometry(1,1,1);
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geomertry, material);
+        scene.add(cube);
+
+        camera.position.z = 2;
+
+        function animate() {
+            requestAnimationFrame( animate );
+            cube.rotation.x += 0.01;
+			cube.rotation.y += 0.01;
+            renderer.render( scene, camera );
+        }
+        animate();
     },[])
     
     
     return (
-        <div className='w-screen h-screen bg-black'>
+        <div className='w-screen h-screen'>
             <canvas ref={canvasRef}/>
         </div>
     )
