6867e07abb1122802c925d101487077d4df4836b
dongmin <dongmin11566@gmail.com>
2024-02-20 17:16:21 +0900

    feat : 3D 우주 모델 추가

--- src/components/Thunder.tsx
+++ src/components/Thunder.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { update } from 'three/examples/jsm/libs/tween.module.js';
 
 export default function Thunder() {
     
@@ -14,7 +13,8 @@ export default function Thunder() {
         const scene = new THREE.Scene();
 
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.z = 2;
+        camera.position.z = 10;
+
 
         const renderer = new THREE.WebGLRenderer({ canvas: canvas , antialias: true});
         renderer.setSize(window.innerWidth, window.innerHeight);
@@ -22,42 +22,39 @@ export default function Thunder() {
         //3D모델 로드
         const loader = new GLTFLoader();
 
-        loader.load( '/shiba/scene.gltf', function ( gltf ) {
+        loader.load('/night_sky_visible_spectrum_monochromatic.glb', function ( gltf ) {
 
             scene.add( gltf.scene );
+            console.log(gltf);
             
-            //모델 회전
-            const initialRotation = new THREE.Euler(0, 0, 0, 'XYZ');
-            gltf.scene.rotation.copy(initialRotation);
-            // 모델의 회전을 갱신하는 함수입니다.
-            function updateRotation() {
-                // 모델의 현재 회전 각도를 가져옵니다.
-                const currentRotation = gltf.scene.rotation.clone();
+            // //모델 회전
+            // const initialRotation = new THREE.Euler(0, 0, 0, 'XYZ');
+            // gltf.scene.rotation.copy(initialRotation);
+            // // 모델의 회전을 갱신하는 함수입니다.
+            // function updateRotation() {
+            //     // 모델의 현재 회전 각도를 가져옵니다.
+            //     const currentRotation = gltf.scene.rotation.clone();
 
-                // x, y 축의 회전 각도를 더합니다.
-                currentRotation.x += 0.005;
-                currentRotation.y += 0.005;
+            //     // x, y 축의 회전 각도를 더합니다.
+            //     currentRotation.x += 0.005;
+            //     currentRotation.y += 0.005;
 
-                // 모델의 회전 각도를 설정합니다.
-                gltf.scene.rotation.copy(currentRotation);
-            }
-            function animate() {
-                requestAnimationFrame( animate );
-                updateRotation();
-                renderer.render( scene, camera );
-            }
-            animate();
+            //     // 모델의 회전 각도를 설정합니다.
+            //     gltf.scene.rotation.copy(currentRotation);
+            // }
+            
 
         }, undefined, function ( error ) {
 
             console.error( error );
 
         } );
-        
 
-        
-
-        
+        function animate() {
+            requestAnimationFrame( animate );
+            renderer.render( scene, camera );
+        }
+        animate();
     },[])
     
     
