21a57132c10cc1f58ad512fe98de06739be96983
dongmin <dongmin11566@gmail.com>
2024-02-28 20:16:49 +0900

    feat : three 컴포넌트가 fixed 일때만 카메라 각이 변하도록 수정

--- src/components/Three.tsx
+++ src/components/Three.tsx
@@ -54,9 +54,10 @@ export default function Three(props:{isfixed:any, setIsFixed: any}) {
             console.error( error );
 
         } );
-        const light = new THREE.DirectionalLight( 0xF2F5A9, 1 );
-        light.position.set( 80, -150, 10 );
+        const light = new THREE.DirectionalLight( 0xffffff, 1);
+        light.position.set(10, -20, 15);
         scene.add( light );
+        
         function animate() {
             requestAnimationFrame( animate );
             if (reactLogoRef.current) {
@@ -65,44 +66,51 @@ export default function Three(props:{isfixed:any, setIsFixed: any}) {
             renderer.render( scene, camera );
         }
         animate();
-
-        // 스크롤 이벤트 핸들러
-        const handleScroll = () => {
-            const scrollTop = document.documentElement.scrollTop;
-            const scrollHeight = document.documentElement.scrollHeight;
-            const clientHeight = document.documentElement.clientHeight;
-            const scrollBottom = scrollHeight - scrollTop - clientHeight;
-            const scrollAngle = scrollTop * 0.003; // 스크롤 위치에 따라 카메라의 회전 각도 변경
-            // cameraRef.current!.rotation.x = scrollAngle;
-            cameraRef.current!.rotation.y = -scrollAngle * 0.1;
-            if(scrollBottom <= 0 || scrollTop === 0){
-                props.setIsFixed(false);
-            }
-        };
-
-        // 스크롤 이벤트 리스너 등록
-        window.addEventListener('scroll', handleScroll);
-
-        // 컴포넌트 언마운트 시 스크롤 이벤트 리스너 제거
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
+        
     },[])
 
-    // 마우스 커서를 따라다님
+    //fixed 일 때에만 카메라각 변경
     useEffect(() => {
-        const handleMouseMove = (event:MouseEvent) => {
-        setMousePosition({ x: event.clientX, y: event.clientY });
-        };
+        if(props.isfixed === true){
+            // 스크롤 이벤트 핸들러
+            const handleScroll = () => {
+                const scrollTop = document.documentElement.scrollTop;
+                const scrollHeight = document.documentElement.scrollHeight;
+                const clientHeight = document.documentElement.clientHeight;
+                const scrollBottom = scrollHeight - scrollTop - clientHeight;
+                const scrollAngle = scrollTop * 0.003; // 스크롤 위치에 따라 카메라의 회전 각도 변경
+                // cameraRef.current!.rotation.x = scrollAngle;
+                cameraRef.current!.rotation.y = -scrollAngle * 0.1;
+                if(scrollBottom <= 0 || scrollTop === 0){
+                    props.setIsFixed(false);
+                }
+            };
+            
+            // 스크롤 이벤트 리스너 등록
+            window.addEventListener('scroll', handleScroll);
+    
+            // 컴포넌트 언마운트 시 스크롤 이벤트 리스너 제거
+            return () => {
+                window.removeEventListener('scroll', handleScroll);
+            };
+            }
+    },[props.isfixed])
 
-        // 이벤트 리스너 등록
-        document.addEventListener('mousemove', handleMouseMove);
+    // // 마우스 커서를 따라다님
+    // useEffect(() => {
+    //     const handleMouseMove = (event:MouseEvent) => {
+    //     setMousePosition({ x: event.clientX, y: event.clientY });
+    //     };
 
-        // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
-        return () => {
-        document.removeEventListener('mousemove', handleMouseMove);
-        };
-    }, [mousePosition]);
+    //     // 이벤트 리스너 등록
+    //     document.addEventListener('mousemove', handleMouseMove);
+
+    //     // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+    //     return () => {
+    //     document.removeEventListener('mousemove', handleMouseMove);
+    //     };
+    // }, [mousePosition]);
+    
     
     
     return (
