30e90c0e3f93b40e717f0f62f50bd276d19c56a5
dongmin <dongmin11566@gmail.com>
2024-03-01 17:50:36 +0900

    feat : 마우스에 커서를 따라 움직이는 원형 그라데이션 추가

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 export default function Project() {
 
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+    const [opacity, setOpacity] = useState(0); // 초기값을 0으로 설정합니다.
     // 마우스 커서를 따라다님
     useEffect(() => {
         const handleMouseMove = (event:MouseEvent) => {
@@ -18,13 +19,22 @@ export default function Project() {
         };
     }, [mousePosition]);
     return (
-        <div className="w-screen h-screen">
-            <h1>Project</h1>
-            <div className="flex flex-row justify-center w-full h-1/2 space-x-10">
-                <div className="basis-1/4 rounded-lg border">1</div>
-                <div className="basis-1/4 rounded-lg border">2</div>
-                <div className="basis-1/4 rounded-lg border">3</div>
+        <>
+        <div className=" w-[25rem] h-[25rem] rounded-full bg-radical-gradient" 
+        style={{
+            position: 'fixed',
+            top: `${mousePosition.y}px`,
+            left: `${mousePosition.x}px`,
+            transform: 'translate(-50%, -50%)',
+        }}/>
+        <div className="w-screen h-screen bg-[#212121] flex flex-col justify-around px-[5%]">
+            <h1 className="text-3xl">Project</h1>
+            <div className="flex flex-row justify-center w-full h-[60%] space-x-6">
+                <div className="basis-1/3 h-full rounded-lg border bg-[#2b2b2b] border-solid border-[#eceff133]"></div>
+                <div className="basis-1/3 h-full rounded-lg border bg-[#2b2b2b] border-solid border-[#eceff133]"></div>
+                <div className="basis-1/3 h-full rounded-lg border bg-[#2b2b2b] border-solid border-[#eceff133]"></div>
             </div>
         </div>
+        </>
     )
 }
