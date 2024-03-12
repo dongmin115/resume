f7404ee0d9db6a2e85566f2fa472fcb660ddba82
dongmin <dongmin11566@gmail.com>
2024-03-12 19:03:31 +0900

    fix : 바뀌지 않는 변수 const 선언으로 변경

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function Project() {
   useEffect(() => {
-    const cardsContainer = document.querySelector(".boxes") as HTMLElement; 
+    const cardsContainer = document.querySelector(".boxes") as HTMLElement;
     const cards = Array.from(document.querySelectorAll(".box"));
     const overlay = document.querySelector(".overlay");
 
@@ -27,30 +27,33 @@ export default function Project() {
     const observer = new ResizeObserver((entries) => {
       entries.forEach((entry) => {
         const cardIndex = cards.indexOf(entry.target as HTMLElement);
-        let width = entry.borderBoxSize[0].inlineSize;
-        let height = entry.borderBoxSize[0].blockSize;
+        const width = entry.borderBoxSize[0].inlineSize;
+        const height = entry.borderBoxSize[0].blockSize;
 
         if (cardIndex >= 0) {
-            const overlayChild = overlay!.children[cardIndex] as HTMLDivElement;
-            if (overlayChild) {
-                overlayChild.setAttribute("style", `width: ${width}px; height: ${height}px;`);
-            }
+          const overlayChild = overlay!.children[cardIndex] as HTMLDivElement;
+          if (overlayChild) {
+            overlayChild.setAttribute(
+              "style",
+              `width: ${width}px; height: ${height}px;`
+            );
+          }
         }
       });
     });
 
     const initOverlayCard = (cardEl: HTMLElement) => {
-        const overlayCard = document.createElement("div");
-        overlayCard.classList.add("box");
-        createOverlayCta(overlayCard, cardEl.lastElementChild as HTMLElement);
-        // overlay 요소에 자식 요소가 없을 때만 자식 요소를 추가합니다.
-        if (overlay!.children.length < 3) {
+      const overlayCard = document.createElement("div");
+      overlayCard.classList.add("box");
+      createOverlayCta(overlayCard, cardEl.lastElementChild as HTMLElement);
+      // overlay 요소에 자식 요소가 없을 때만 자식 요소를 추가합니다.
+      if (overlay!.children.length < 3) {
         overlay!.append(overlayCard);
-    }
+      }
 
       observer.observe(cardEl);
     };
-    
+
     cards.forEach((cardEl) => initOverlayCard(cardEl as HTMLElement));
     document.body.addEventListener("pointermove", applyOverlayMask);
 
@@ -64,33 +67,46 @@ export default function Project() {
       <h1 className="main__heading">Project</h1>
       <div className="main__boxes boxes">
         <div className="boxes__inner">
-        {/* 1번째 프로젝트 */}
+          {/* 1번째 프로젝트 */}
           <div className="boxes__box box">
             <div className="flex flex-row items-center justify-between">
-            <h2 className="box__heading">Clip Tab!</h2>
-            <h1 className="box_bullets">2023.12 ~ 2024.02</h1>
+              <h2 className="box__heading">Clip Tab!</h2>
+              <h1 className="box_bullets">2023.12 ~ 2024.02</h1>
             </div>
-            <p className="box__price">유용한 북마크 관리, url 이미지 추출 익스텐션</p>
+            <p className="box__price">
+              유용한 북마크 관리, url 이미지 추출 익스텐션
+            </p>
             <ul role="list" className="box__bullets flow">
-                <li>프로젝트에 기여하고 사용한 기술 설명</li>
-                <li><a href="https://medium.com/@vitash1215/2023-siliconvalley-winter-bootcamp-cliptab-korean-755b3448266b">Medium</a></li>
+              <li>프로젝트에 기여하고 사용한 기술 설명</li>
+              <li>
+                <a href="https://medium.com/@vitash1215/2023-siliconvalley-winter-bootcamp-cliptab-korean-755b3448266b">
+                  Medium
+                </a>
+              </li>
             </ul>
-            <a href="https://github.com/2023-WinterBootcamp-Team-M" className="box__cta cta">GitHub</a>
+            <a
+              href="https://github.com/2023-WinterBootcamp-Team-M"
+              className="box__cta cta"
+            >
+              GitHub
+            </a>
           </div>
-        {/* 2번째 프로젝트 */}
+          {/* 2번째 프로젝트 */}
           <div className="boxes__box box">
-            <h2 className="box__heading">졸업작품</h2>
+            <h2 className="box__heading">DreamVault</h2>
             <p className="box__price">개발기간</p>
             <ul role="list" className="box__bullets flow">
               <li>Access to advanced workouts and nutrition plans</li>
               <li>Priority Email support</li>
               <li>Exclusive access to live Q&A sessions</li>
             </ul>
-            <a href="#pro" className="box__cta cta">Upgrade to Pro</a>
+            <a href="#pro" className="box__cta cta">
+              Upgrade to Pro
+            </a>
           </div>
-        {/* 3번째 프로젝트 */}
+          {/* 3번째 프로젝트 */}
           <div className="boxes__box box">
-            <h2 className="box__heading">테커 상반기 프로젝트</h2>
+            <h2 className="box__heading">On My Desk</h2>
             <p className="box__price">개발기간</p>
             <ul role="list" className="box__bullets flow">
               <li>Access to all premium workouts and nutrition plans</li>
@@ -98,10 +114,12 @@ export default function Project() {
               <li>1-on-1 virtual coaching session every month</li>
               <li>Exclusive content and early access to new features</li>
             </ul>
-            <a href="#ultimate" className="box__cta cta">Go Ultimate</a>
+            <a href="#ultimate" className="box__cta cta">
+              Go Ultimate
+            </a>
           </div>
         </div>
-        
+
         <div className="overlay boxes__inner"></div>
       </div>
     </main>
