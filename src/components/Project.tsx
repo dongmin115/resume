3b3641399ea9793aebe35b6763ceecc6aa74d7c5
dongmin <dongmin11566@gmail.com>
2024-06-20 03:19:40 +0900

    style: Project 내용 수정

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -73,16 +73,10 @@ export default function Project() {
               <h2 className="box__heading">Clip Tab!</h2>
               <h1 className="box_bullets">2023.12 ~ 2024.02</h1>
             </div>
-            <p className="box__price">
-              유용한 북마크 관리, url 이미지 추출 익스텐션
-            </p>
+            <p className="box__price">북마크 관리, url 이미지 추출 익스텐션</p>
             <ul role="list" className="box__bullets flow">
-              <li>프로젝트에 기여하고 사용한 기술 설명</li>
-              <li>
-                <a href="https://medium.com/@vitash1215/2023-siliconvalley-winter-bootcamp-cliptab-korean-755b3448266b">
-                  Medium
-                </a>
-              </li>
+              <li>드래그 앤 드롭 기능구현</li>
+              <li>무한 스크롤 기능구현</li>
             </ul>
             <a
               href="https://github.com/2023-WinterBootcamp-Team-M"
@@ -93,29 +87,36 @@ export default function Project() {
           </div>
           {/* 2번째 프로젝트 */}
           <div className="boxes__box box">
-            <h2 className="box__heading">DreamVault</h2>
-            <p className="box__price">AI가 생성한 음악 저장소</p>
+            <div className="flex flex-row items-center justify-between">
+              <h2 className="box__heading">DreamVault</h2>
+              <h1 className="box_bullets">2023.03 ~ 2024.06</h1>
+            </div>
+            <p className="box__price">AI가 생성한 음악 스트리밍 플랫폼</p>
             <ul role="list" className="box__bullets flow">
-              <li>Access to advanced workouts and nutrition plans</li>
-              <li>Priority Email support</li>
-              <li>Exclusive access to live Q&A sessions</li>
+              <li>액세스 토큰 만료 대응 로직 구현</li>
+              <li>음악 스트리밍 기능구현</li>
+              <li>반응형 디자인 적용</li>
             </ul>
-            <a href="#pro" className="box__cta cta">
-              Upgrade to Pro
+            <a
+              href="https://github.com/DreamVault-2024"
+              className="box__cta cta"
+            >
+              GitHub
             </a>
           </div>
           {/* 3번째 프로젝트 */}
           <div className="boxes__box box">
-            <h2 className="box__heading">On My Desk</h2>
+            <div className="flex flex-row items-center justify-between">
+              <h2 className="box__heading">On My Desk</h2>
+              <h1 className="box_bullets">2024.03 ~ 2024.06</h1>
+            </div>
             <p className="box__price">데스크탑 셋업 공유 플랫폼</p>
             <ul role="list" className="box__bullets flow">
-              <li>Access to all premium workouts and nutrition plans</li>
-              <li>24/7 Priority support</li>
-              <li>1-on-1 virtual coaching session every month</li>
-              <li>Exclusive content and early access to new features</li>
+              <li>텍스트 에디터 기능구현</li>
+              <li>폼관리 및 유효성 검증 로직 구현</li>
             </ul>
-            <a href="#ultimate" className="box__cta cta">
-              Go Ultimate
+            <a href="https://github.com/Team-OMD" className="box__cta cta">
+              GitHub
             </a>
           </div>
         </div>
